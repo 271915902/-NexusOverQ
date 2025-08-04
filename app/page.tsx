087bb209e5e1a4b184157a66ab01ea725e58bab3
271915902 <271915902@qq.com>
2025-08-04 11:50:46 +0800

    feat: 添加导航栏组件和下拉菜单功能
    - 创建导航栏组件并添加到布局中
    - 添加Radix UI下拉菜单组件
    - 更新全局CSS样式和主题变量
    - 添加网站Logo资源
    - 移除首页占位文本

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello next.js</h1>
+      <h1></h1>
     </div>
   );
 }
---
+++ components/navigation/navbar/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+function Navbar() {
+  return (
+    <div
+      className="flex-between background-light900_dark200 
+    fixed 
+    z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12"
+    >
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/images/site-logo.svg"
+          alt="Nexus Over Q"
+          width={23}
+          height={23}
+        />
+
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Nexus<span className="text-primary-500">OverQ</span>
+        </p>
+      </Link>
+      <p>search</p>
+
+      <div className="flex-between gap-5">Theme</div>
+    </div>
+  );
+}
+
+export default Navbar;
